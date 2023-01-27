97ea84d2b749b48574f07105a70d29c5abca5c18
manishrajbhar1998 <manishrajbhar379@gmail.com>
2023-01-27 23:46:50 +0530

    First new methode added

--- First.java
+++ First.java
@@ -2,4 +2,8 @@ class First{
     public static void main(String[] argd){
         System.out.print("Hello world")
     }
+    
+    public void getData(){
+        System.out.print("Get Data");
+    }
 }
