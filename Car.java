3b0edc62d563d2449f6791d74ac4e76fb96d962f
manishrajbhar1998 <manishrajbhar379@gmail.com>
2023-01-28 13:18:49 +0530

    Car object Created

---
+++ Car.java
@@ -0,0 +1,15 @@
+class Car{
+	
+	String brand = "BMW";
+	int price = 2000000;
+	
+	
+	public static void main(String[] args){
+
+		Car obj = new Car();
+
+		System.out.println("Hello world");
+		System.out.println(brand);
+		System.out.println(obj.price);
+	}
+}
--- First.java
+++ First.java
@@ -1,9 +1,28 @@
 class First{
-    public static void main(String[] argd){
-        System.out.print("Hello world")
-    }
-    
-    public void getData(){
-        System.out.print("Get Data");
-    }
+	public static void main(String[] args){
+
+		System.out.println("Hello world 1");
+		First.main();
+		First obj = new First();
+		obj.main();
+
+	}
+
+	public static void main(){
+
+		System.out.println("Hello world 2");
+		
+	}
+
+	
+	
+
+}
+
+class Second{
+
+	public void main(){
+
+		System.out.println("Hello world 2");
+	}
 }
